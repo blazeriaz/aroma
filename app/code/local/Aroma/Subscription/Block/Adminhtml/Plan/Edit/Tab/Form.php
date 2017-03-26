7775aa25e14f9f38e2b5eeacb1468377c76cd7d4
vasanthBR <phpvasanth@yahoo.com>
2017-03-26 19:08:24 +0530

    store config

--- app/code/local/Aroma/Subscription/Block/Adminhtml/Plan/Edit/Tab/Form.php
+++ app/code/local/Aroma/Subscription/Block/Adminhtml/Plan/Edit/Tab/Form.php
@@ -3,6 +3,7 @@ class Aroma_Subscription_Block_Adminhtml_Plan_Edit_Tab_Form extends Mage_Adminht
 {
     protected function _prepareForm()
     {
+		
         $form = new Varien_Data_Form();
 
         $fieldset = $form->addFieldset('plan_form', array('legend'=>Mage::helper('adminhtml')->__('Plan information')));
@@ -27,19 +28,18 @@ class Aroma_Subscription_Block_Adminhtml_Plan_Edit_Tab_Form extends Mage_Adminht
             'required'  => true,
             'name'      => 'no_of_ship',
         ));
-		
+		$base_price = Mage::getStoreConfig('subscription/subscription/basic');
+		$deluxe_price = Mage::getStoreConfig('subscription/subscription/deluxe');
 		$fieldset->addField('price', 'text', array(
             'label'     => Mage::helper('adminhtml')->__('Offer in %'),
             'class'     => 'required-entry validate-number validate-digits-range digits-range-0-99',
             'required'  => true,
-			'onchange'	=> 'setPriceOnOffer(this.value)',
+			'onchange'	=> 'setPriceOnOffer('.$base_price.','.$deluxe_price.')',
             'name'      => 'price'
         ))->setAfterElementHtml("<p id='offer_price' style='display:none;'>Base Price - <small id='base_price'></small><br>
 								Deluxe Price - <small id='deluxe_price'></small><br></p>
                          <script type=\"text/javascript\">
-                            function setPriceOnOffer(selectElement){ 
-								var default_base_price = 18.99;
-								var default_deluxe_price = 21.99;
+                            function setPriceOnOffer(default_base_price,default_deluxe_price){ 
 								var offer_percentage = $('price').value;
 								var base_price = (default_base_price * offer_percentage)/100;
 								var deluxe_price = (default_deluxe_price * offer_percentage)/100;
