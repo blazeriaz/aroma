b943a1d462e39e9683347176445ad1a81f11eb55
vasanthBR <phpvasanth@yahoo.com>
2017-03-27 23:40:27 +0530

    Subscription list

---
+++ app/code/local/Aroma/Subscription/controllers/Adminhtml/ListController.php
@@ -0,0 +1,22 @@
+<?php
+ 
+class Aroma_Subscription_Adminhtml_ListController extends Mage_Adminhtml_Controller_Action
+{
+	protected function _initAction()
+    {
+        $this->loadLayout()
+            ->_setActiveMenu('subscription/index')
+            ->_addBreadcrumb(Mage::helper('salesrule')->__('Subscription'), Mage::helper('salesrule')->__('List'))
+        ;
+        return $this;
+    }
+    public function indexAction()
+    {		
+        $this->_title($this->__('Subscription'))->_title($this->__('List'));
+        $this->loadLayout();
+		$this->_setActiveMenu('subscription/index');
+		$this->_addContent($this->getLayout()->createBlock('aroma_subscription/adminhtml_list'));
+        $this->renderLayout();
+    }
+}
+?>
