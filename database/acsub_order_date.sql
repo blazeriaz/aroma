805ed0f91c36d521280725d1b89a4922cc8fc602
vasanthBR <phpvasanth@yahoo.com>
2017-03-29 22:44:17 +0530

    subscription

---
+++ database/acsub_order_date.sql
@@ -0,0 +1,34 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 29, 2017 at 07:12 PM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aroma_new`
+--
+
+--
+-- Dumping data for table `acsub_order_date`
+--
+
+INSERT INTO `acsub_order_date` (`id`, `eav_sub_id`, `ship_date`, `created`, `modified`, `status`) VALUES
+(1, 1, '2017-03-30 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1),
+(2, 2, '2017-03-31 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1),
+(3, 1, '2017-04-01 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 2);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
